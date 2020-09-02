16fa7df63024fbede80ae63d93605d4bfbd9aa33
Viana2021 <caiomarcoschaves@gmail.com>
2020-09-02 12:39:17 -0300

    Code modularized

--- ex1/ex1.cpp
+++ ex1/ex1.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 using namespace std;
 
+#define NOT_SINGLE -1
+
 struct __person
 {
 	enum STATUS
@@ -11,9 +13,43 @@ struct __person
 		MARRIED
 	}status;
 	vector<int> pref;
+	int pair;
 };
 
 typedef struct __person PERSON;
+
+void read_input(vector<PERSON> man, vector<PERSON> woman)
+{
+	int i, j;
+	int marriages;
+	int aux;
+	cin >> marriages;
+	for(i = 0; i < marriages; i++)
+	{
+		cin >> aux;
+		PERSON w;
+		woman.push_back(w);
+		for(j = 0; j < marriages; j++)
+		{
+			cin >> aux;
+			woman[i].pref.push_back(aux);
+			woman[i].status = PERSON::SINGLE;
+		}
+
+	}
+	for(i = 0; i < marriages; i++)
+	{
+		cin >> aux;
+		PERSON m;
+		man.push_back(m);
+		for(j = 0; j < marriages; j++)
+		{
+			cin >> aux;
+			man[i].pref.push_back(aux);
+			man[i].status = PERSON::SINGLE;
+		}
+	}
+}
 
 void debug(vector<PERSON> man, vector<PERSON> woman)
 {
@@ -42,77 +78,55 @@ void debug(vector<PERSON> man, vector<PERSON> woman)
 	}
 }
 
-bool isEveryManMarried(vector<PERSON> man)
+int isEveryManMarried(vector<PERSON> man)
 {
-	bool isEveryoneMarried = true;
-	//for(i = 0; i < man.size() && isEveryoneMarried; i++)
-	for(auto m : man)
+	bool single = true;
+	int i;
+	for(i = 0; i < man.size() && single; i++)
 	{
-		if(m.status == PERSON::SINGLE) isEveryoneMarried = false;
+		if(man[i].status == PERSON::SINGLE) single = false;
 	}
-	return isEveryoneMarried;
+	if(single == true) i = NOT_SINGLE + 1;
+	return i - 1;
+}
+
+bool isAvailable(vector<PERSON> person)
+{
+	bool isAvailable;
+
+	return isAvailable;
+}
+
+void __propose(PERSON man, vector<PERSON> woman)
+{
 }
 
 void propose(vector<PERSON> man, vector<PERSON> woman)
 {
-	int counter = 0;
-	while(!isEveryManMarried(man))
+	int index = isEveryManMarried(man);
+	while(index != NOT_SINGLE)
 	{
-		if(man[counter].status == PERSON::SINGLE)
+		for(PERSON m : man)
 		{
-			//TODO
-			//Needs to think how to create the marriages
-			//Very tired right now :p
+			__propose(m, woman);
 		}
+		index = isEveryManMarried(man);
 	}
 }
 
 int main(void)
 {
 	int tests;
-	int marriages;
-	int i, j;
-	int aux;
-	//PERSON *man = new PERSON [500];
-	//PERSON *woman = new PERSON [500];
 	vector<PERSON> man;
 	vector<PERSON> woman;
-	//PERSON m, w;
 
 	cin >> tests;
-	//while(cont < tests)
 	while(tests--)
 	{
-		cin >> marriages;
-		int cont;
-		for(i = 0; i < marriages; i++)
-		{
-			cin >> aux;
-			PERSON w;
-			woman.push_back(w);
-			for(j = 0; j < marriages; j++)
-			{
-				cin >> aux;
-				woman[i].pref.push_back(aux);
-				woman[i].status = PERSON::SINGLE;
-			}
-
-		}
-		for(i = 0; i < marriages; i++)
-		{
-			cin >> aux;
-			PERSON m;
-			man.push_back(m);
-			for(j = 0; j < marriages; j++)
-			{
-				cin >> aux;
-				man[i].pref.push_back(aux);
-				man[i].status = PERSON::SINGLE;
-			}
-		}
+		read_input(man, woman);
 		propose(man, woman);
 		//debug(man, woman);
-		cont++;
+		//print_results(man, woman);
 		man.clear();
 		woman.clear();
 	}
